87b29ffa4f7eab760dc9bda4bd6fd152bd2a0529
Morten Scheel <morten@mortenscheel.com>
2016-07-15 21:59:14 +0200

    Arkanoid fun :)

--- game.js
+++ game.js
@@ -4,9 +4,9 @@
 
 var Game = function (params) {
 	var self = this;
-	self.fps = params.fps || 60;
-	self.board = new Board(params.canvas);
-	self.gameLoop = {
+	this.fps = params.fps || 60;
+	this.board = new Board(params.canvas);
+	this.gameLoop = {
 		start: function () {
 			self.gameLoop.startedAt = Game.ts();
 			self.gameLoop.previous = self.gameLoop.startedAt;
@@ -30,20 +30,20 @@ var Game = function (params) {
 			clearInterval(self.gameLoop.fpsTimer);
 		}
 	}
-	self.update = function (delta) {
+	this.update = function (delta) {
 		self.board.update(delta);
 	}
-	self.render = function () {
+	this.render = function () {
 		self.board.render();
 	}
-	self.start = function () {
+	this.start = function () {
 		self.gameLoop.start();
 		self.board.init();
 	}
-	self.stop = function () {
+	this.stop = function () {
 		self.gameLoop.stop();
 	}
-	self.dump = function () {
+	this.dump = function () {
 		console.log(self);
 	}
 }
@@ -57,23 +57,21 @@ Game.rnd = function (max, round) {
 
 var Board = function (canvas_id) {
 	var self = this;
-	self.canvas = document.getElementById(canvas_id);
-	self.context = self.canvas.getContext('2d');
-	self.width = self.canvas.width;
-	self.height = self.canvas.height;
-	self.entities = []
-	self.update = function (delta) {
-		var debugMessage = "";
+	this.canvas = document.getElementById(canvas_id);
+	this.context = this.canvas.getContext('2d');
+	this.width = this.canvas.width;
+	this.height = this.canvas.height;
+	this.entities = [];
+	this.state = 'aim';
+	this.update = function (delta) {
 		for (var i in self.entities) {
 			if (self.entities.hasOwnProperty(i)) {
 				var entity = self.entities[i];
 				entity.update(delta);
-				debugMessage += entity.name + ': (' + Math.floor(entity.x) + ', ' + Math.floor(entity.y) + ')\n';
-				if (entity.x <= 0 || entity.x + entity.w >= self.width) {
-					entity.dx *= -1;
-				}
-				if (entity.y <= 0 || entity.y + entity.h >= self.width) {
-					entity.dy *= -1;
+				if (!entity.alive){
+					// remove
+					self.entities.splice(self.entities.indexOf(entity), 1);
+					continue;
 				}
 				for (var j in self.entities) {
 					if (self.entities.hasOwnProperty(j)) {
@@ -82,19 +80,14 @@ var Board = function (canvas_id) {
 							continue;
 						}
 						if (entity.collidesWith(otherEntity)) {
-							entity.dx *= -1;
-							entity.dy *= -1;
-							//otherEntity.dx *= -1;
-							//otherEntity.dy *= -1;
-							console.log('collission at ' + entity.x + ', ' + entity.y);
+							entity.onCollission(otherEntity);
 						}
 					}
 				}
 			}
 		}
-		//$("#debug").html(debugMessage);
 	}
-	self.render = function () {
+	this.render = function () {
 		self.context.clearRect(0, 0, self.width, self.height);
 		for (var i in self.entities) {
 			if (self.entities.hasOwnProperty(i)) {
@@ -103,54 +96,125 @@ var Board = function (canvas_id) {
 			}
 		}
 	}
-	self.init = function () {
-		self.canvas.addEventListener('click', function (event) {
-			var r      = Game.rnd(255, true),
-			    g      = Game.rnd(255, true),
-			    b      = Game.rnd(255, true),
-			    a      = 1, //0.5 + Game.rnd(0.5, true),
-			    entity = new Entity({
-				    context: self.context,
-				    x      : event.layerX,
-				    y      : event.layerY,
-				    w      : Game.rnd(100, true),
-				    h      : Game.rnd(100, true),
-				    dx     : Game.rnd(0.1),
-				    dy     : Game.rnd(0.1),
-				    color  : 'rgba(' + r + ',' + g + ',' + b + ',' + a + ')'
-			    });
-			if (Game.rnd(2) > 1){
-				entity.dx *= -1;
+	this.init = function () {
+		// Paddle
+		var paddle = new Entity({
+			x      : 100,
+			y      : self.height - 50,
+			w      : 150,
+			h      : 10,
+			dx     : 0,
+			dy     : 0,
+			context: self.context,
+			color  : '#fff'
+		});
+		self.entities.push(paddle);
+		self.canvas.addEventListener('mousemove', function (event) {
+			var x         = event.layerX,
+			    halfWidth = paddle.w / 2;
+			if (x > halfWidth && x < self.width - halfWidth) {
+				paddle.x = x - halfWidth;
 			}
-			if (Game.rnd(2) > 1){
-				entity.dy *= -1;
+		});
+		// Ball
+		var ball = new Entity({
+			w      : 10,
+			h      : 10,
+			dx     : 0,
+			dy     : 0,
+			context: self.context,
+			color  : '#0f0'
+		});
+		self.entities.push(ball);
+		self.canvas.addEventListener('mousemove', function () {
+			if (self.state == 'aim') {
+				ball.x = paddle.x + (paddle.w / 2 - ball.w / 2);
+				ball.y = paddle.y - ball.h;
 			}
-			console.log('entity added', entity);
-			self.entities.push(entity)
-		})
+		});
+		ball.update = function (delta) {
+			if (self.state == 'flying') {
+				// hit ground
+				if (ball.y >= self.height - ball.h) {
+					alert('you died');
+					ball.dx = 0;
+					ball.dy = 0;
+					self.state = 'aim';
+				}
+				// hit ceiling
+				if (ball.y <= ball.h) {
+					ball.dy *= -1;
+				}
+				// hit wall
+				if (ball.x <= ball.w || ball.x >= self.width - ball.w) {
+					ball.dx *= -1;
+				}
+				ball.x += ball.dx * delta;
+				ball.y += ball.dy * delta;
+			}
+		}
+		ball.onCollission = function () {
+			ball.dy *= -1;
+		}
+		// launch ball
+		self.canvas.addEventListener('mouseup', function () {
+			if (self.state == 'aim') {
+				console.log('go');
+				self.state = 'flying';
+				// random horizontal direction
+				ball.dx = Game.rnd(1) - 0.5;
+				ball.dy = -0.3;
+			}
+		});
+
+		// Bricks
+		var wrapperWidth = self.width / 6,
+		    margin       = wrapperWidth / 10,
+		    brickWidth   = wrapperWidth * 0.8;
+		for (var i = 0; i < 6; i++) {
+			for (var j = 0 ; j < 4 ; j++){
+				var brick = new Entity({
+					x      : i * wrapperWidth + margin,
+					y      : 50 + j * 50,
+					w      : brickWidth,
+					h      : 30,
+					context: self.context,
+					color  : '#f4b',
+					onCollission: function () {
+						this.alive = false;
+					}
+				});
+				self.entities.push(brick);
+			}
+		}
 	}
 }
 
 var Entity = function (params) {
 	var self = this;
-	self.name = params.name || 'No name';
-	self.context = params.context;
-	self.x = params.x || 0;
-	self.y = params.y || 0;
-	self.w = params.w || 10;
-	self.h = params.h || 10;
-	self.dx = params.dx || 0;
-	self.dy = params.dy || 0;
-	self.color = params.color || '#000';
-	self.update = function (delta) {
+	this.alive = true;
+	this.name = params.name || 'No name';
+	this.context = params.context;
+	this.x = params.x || 0;
+	this.y = params.y || 0;
+	this.w = params.w || 10;
+	this.h = params.h || 10;
+	this.dx = params.dx || 0;
+	this.dy = params.dy || 0;
+	this.color = params.color || '#000';
+	this.onCollission = params.onCollission || function (other) {
+		self.dx *= -1;
+		self.dy *= -1;
+	}
+	this.update = function (delta) {
 		self.x += self.dx * delta;
 		self.y += self.dy * delta;
 	}
-	self.render = function () {
+	this.render = function () {
 		self.context.fillStyle = self.color;
 		self.context.fillRect(self.x, self.y, self.w, self.h);
 	}
-	self.collidesWith = function (other) {
+	this.collidesWith = function (other) {
 		return self.x + self.w >= other.x && self.x <= other.x + other.w && self.y + self.h >= other.y && self.y <= other.y + other.h;
 	}
 }
